db13a6bb1e86936632a94d6c4646c65ac860fa0f
PrachiSharmaPS <prachisharma.ps01@gmail.com>
2022-11-28 14:33:06 +0530

    start of project

---
+++ src/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+var bodyParser = require('body-parser');
+const mongoose = require('mongoose')
+//const route = require('./routes/route.js');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect("mongodb+srv://vishwasw75:9595335675@firstcluster.jde07cq.mongodb.net/group12Database", {
+    useNewUrlParser: true
+})
+.then( () => console.log("MongoDb is connected"))
+.catch ( err => console.log(err) )
+
+//app.use('/', route);
+
+app.listen(process.env.PORT || 3000, function() {
+	console.log('Express app running on port ' + (process.env.PORT || 3000))
+});
+
---
+++ src/models/userModel.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose')
+
+
+const userSchema = new mongoose.Schema({
+
+    title: {
+        type: String,
+        enum: ["Mr", "Mrs", "Miss"],
+        required: true
+    },
+    name: {
+         type: String,
+         required: true 
+        },
+
+    phone: {
+        type : String, 
+        required: true, 
+        unique: true 
+        },
+
+    email:  { 
+        type : String,
+         required:true
+        },
+      
+    password:{  
+        type: String, 
+        required: true,
+     
+        },
+    address: {
+        street : {type: String},
+        city : {type: String},
+        pincode: {type: String},
+
+    }
+
+}, { timestamps: true })
+
+
+
+module.exports = mongoose.model('Createuser', userSchema)
